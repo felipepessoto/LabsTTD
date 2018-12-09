ac6dcbb9c9e354d3f478b8874fa0dd266c057294
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2018-12-09 18:05:58 -0200

    TTD Lab

--- LabWindbgTTD/Form1.Designer.cs
+++ LabWindbgTTD/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtLog = new System.Windows.Forms.TextBox();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtLog
+            // 
+            this.TxtLog.Location = new System.Drawing.Point(12, 60);
+            this.TxtLog.Multiline = true;
+            this.TxtLog.Name = "TxtLog";
+            this.TxtLog.Size = new System.Drawing.Size(493, 243);
+            this.TxtLog.TabIndex = 0;
+            this.TxtLog.Text = resources.GetString("TxtLog.Text");
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(13, 31);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 1;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(517, 315);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.TxtLog);
+            this.Name = "Form1";
+            this.Text = "Felipe Fujiy Pessoto - Lab Windbg TTD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtLog;
+        private System.Windows.Forms.Button BtnSave;
     }
 }
 
--- LabWindbgTTD/Form1.cs
+++ LabWindbgTTD/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace LabWindbgTTD
 {
     public partial class Form1 : Form
     {
+        private StreamWriter LogFile { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            LogFile = new StreamWriter("LogLabTDD" + Guid.NewGuid().ToString("N") + ".txt");
+            
+        }
+
+        private async void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await LogFile.WriteAsync(TxtLog.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            await Task.Run(BadCodeClosesFileUnexpectedly);
+        }
+
+        private async Task BadCodeClosesFileUnexpectedly()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            LogFile.Dispose();
         }
     }
 }
